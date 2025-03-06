4a94bea28d1da4b77d00f8e8858982d6f3d2393b
AnjanaSKamath <98830829+AnjanaSKamath@users.noreply.github.com>
2025-03-06 17:38:07 +0530

    Random Number

---
+++ Random.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.Math;
+public class Random{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n,temp,count=0,s=0,rem=0;
+         int max=400,min=200;
+        double a=Math.random()*(max-min+1)+min ;
+        
+        System.out.println(a);
+}
+}
