a796af90f55cf8b9e50b315780e06605dace04e8
AnjanaSKamath <98830829+AnjanaSKamath@users.noreply.github.com>
2025-03-07 16:10:56 +0530

    Sunny Number

---
+++ SunnyNumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class SunnyNumber
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter the number");
+	    int n=sc.nextInt();
+	    int n1=n+1;
+	    double sq=Math.sqrt(n1);
+	    if((sq-Math.floor(sq))==0){
+	       System.out.println(n+" is a sunny number");
+	   }
+	   else
+	   {
+	       System.out.println(n+" is not a sunny number");
+	   }
+	}
+}
