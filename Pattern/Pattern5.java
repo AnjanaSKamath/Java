064638fea05f4de8f2b20028a2729d4259ab2eee
AnjanaSKamath <98830829+AnjanaSKamath@users.noreply.github.com>
2025-03-11 11:23:49 +0530

    Pattern 5

---
+++ Pattern/Pattern5.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Pattern5
+{
+	public static void main(String[] args)
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int n;
+	    System.out.println("Enter the number of rows");
+	    n=sc.nextInt();
+	    for(int i=1;i<=n;i++)
+	    {
+	        for(int j=1;j<=n;j++)
+	        {
+	            if(j<=n-i+1)
+	            {
+	                System.out.print(j);
+	            }
+	            else{
+	                System.out.print("*");
+	            }
+	        }
+	        for(int j=(n+1);j<=(2*n);j++)
+	        {
+	            if(j>=n+i)
+	            {
+	                System.out.print((2*n)+1-j);
+	            }
+	            else
+	            {
+	                System.out.print("*");
+	            }
+	        }
+	        System.out.println();
+	    }
+	}
+}
